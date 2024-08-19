3be5850b1eee983573a8dedeb4bd1296e6f607df
olikelly00 <oliverkelly1995@hotmail.co.uk>
2024-08-19 20:36:05 +0100

    project setup

---
+++ app.js
@@ -0,0 +1,52 @@
+import express  from 'express';
+import helmet from 'helmet';
+
+const app = express();
+const port = 3000;
+
+import { getAllQuotes } from "./libs/quotes.js";
+
+app.use(express.json())
+
+app.use(helmet());
+
+
+
+/* 
+This handler function sets up a welcome message to display on our landing page
+*/
+
+app.get('/', (req, res) => {
+    res.send("Hello world! Welcome to the That's What She Said API")
+});
+
+
+
+/* 
+This listener tells our Express.js app to start listening for incoming requests on a specific port.
+*/
+
+app.listen(port, () => {
+    console.log(`We are now listening on port ${port}`)
+})
+
+/* 
+This handler function returns either:
+• the array of all activities 
+• an error message including a status code and explanation 
+*/
+
+
+app.get("/quotes", async (req, res) => {
+    try {
+        const payload = await getAllQuotes();
+        res.status(200).json({
+            "success": true,
+            "payload": payload
+        });
+    } catch (error) {
+        res.status(404).json({
+            "error": error.message
+        });
+    }
+});
---
+++ libs/quotes.js
@@ -0,0 +1,7 @@
+import quotes from "../data/quotes.json" assert { type: 'json' };
+
+/* This function returns an array of all quotes */
+
+export async function getAllQuotes() {
+  return quotes
+};
